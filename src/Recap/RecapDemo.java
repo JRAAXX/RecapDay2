db9f8d79ede68d1482a711f81e049d332ca2ebae
JRAAXX <140764985+JRAAXX@users.noreply.github.com>
2023-10-20 14:29:59 -0400

    Update RecapDemo.java

--- src/Recap/RecapDemo.java
+++ src/Recap/RecapDemo.java
@@ -5,5 +5,9 @@ public class RecapDemo {
 	public static void main(String [] args) {
 		System.out.println("Recap...");
 	}
+	public void subtract ( double a, double b, double c) {
+		System.out.println(a - b - c );
+		
+	}
 
 }
